01ef6da80a4f7ed3c2cefef2b130f5e937df39e1
siro <zhengchengshanqi@gmail.com>
2025-05-30 14:58:30 +0900

    test: Tailwind class 動作確認 + lint調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="bg-cyan-500">Vite + React test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,8 +28,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="bg-cyan-500 text-white text-3xl font-bold px-4 py-2 rounded">
+          Tailwind テスト中！
+        </h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
